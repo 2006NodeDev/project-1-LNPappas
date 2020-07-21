7a1f3fb70552e63d4c9b64eea8680fdc281622dd
LNPappas <LPappas@gmail.com>
2020-07-21 08:34:32 -0700

    athentication fix??

--- Express-Server/src/messaging/index.ts
+++ Express-Server/src/messaging/index.ts
@@ -1,8 +1,8 @@
 import { PubSub } from '@google-cloud/pubsub'
 // $ npm install @google-cloud/pubsub
 const pubSubClient = new PubSub({
-    // projectId: "unique-perigee-279818",
-    // keyFilename: "C:/Users/wooho/OneDrive/Documents/Revature/example-project1_service_account_key/unique-perigee-279818-52ded1e9a8b4.json"
+    projectId: "unique-perigee-279818",
+    keyFilename: "../key.json"
 })
   
 export const userTopic = pubSubClient.topic('projects/unique-perigee-279818/topics/project1')
