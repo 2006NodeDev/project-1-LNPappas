e48e447039744b682f13e1b1424bd6931902d21f
LNPappas <LPappas@gmail.com>
2020-07-21 09:26:07 -0700

    final fix (fingers crossed)

--- Express-Server/src/messaging/index.ts
+++ Express-Server/src/messaging/index.ts
@@ -1,8 +1,5 @@
 import { PubSub } from '@google-cloud/pubsub'
 // $ npm install @google-cloud/pubsub
-const pubSubClient = new PubSub({
-    projectId: "unique-perigee-279818",
-    keyFilename: "key.json"
-})
+const pubSubClient = new PubSub()
   
 export const userTopic = pubSubClient.topic('projects/unique-perigee-279818/topics/project1')
